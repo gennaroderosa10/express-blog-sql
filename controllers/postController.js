6f042864c4415ef11a663d48c8a5fddf088203a0
gennaroderosa10 <gennaroderosa5@gmail.com>
2026-01-16 16:26:31 +0100

    feat: INDEX done

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,21 @@
 import postsArray from "../data/data.js"
-
+import connection from "../data/db.js";
 
 function index(req, res) {
-    const risposta = {
-        count: postsArray.length,
-        results: postsArray
-    }
-    res.json(risposta)
+    const query = "SELECT * FROM `posts`";
+
+    connection.query(query, (err, result) => {
+        if (err) {
+            res.status(500);
+            return res.json({
+                message: "server error",
+            })
+        }
+
+        res.json({
+            results: result,
+        })
+    });
 };
 
 function show(req, res) {
